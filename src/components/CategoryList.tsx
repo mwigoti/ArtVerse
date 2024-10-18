516d368e501e11403d97e014e691e6611e7b3b2b
mwigoti <henrymwoha02@gmail.com>
2024-10-19 01:16:44 +0300

    footer

--- src/components/CategoryList.tsx
+++ src/components/CategoryList.tsx
@@ -4,8 +4,68 @@ import Image from 'next/image';
 
 const CategoryList = () => {
   return (
-    <div className='px-4  overflow-x-scroll'>
+    <div className='px-4  overflow-x-scroll scrollbar-hide'>
       <div className='flex gap-4 md:gap-8'>
+        <Link href="/list?category=test" className='flex-shrink-0 w-full sm:w-1/2 lg:w-1/4 xl:w-1/6'>
+          <div className='relative bg-slate-100 w-full h-96'>
+            <Image
+              src="/img6.webp"
+              alt="Category Name"
+              fill
+              sizes="20vw"
+              className="object-cover rounded-md"
+            />
+          </div>
+          <h1 className='mt-4 font-light text-cl tracking-wide text-center'>Category Name</h1>
+        </Link>
+        <Link href="/list?category=test" className='flex-shrink-0 w-full sm:w-1/2 lg:w-1/4 xl:w-1/6'>
+          <div className='relative bg-slate-100 w-full h-96'>
+            <Image
+              src="/img6.webp"
+              alt="Category Name"
+              fill
+              sizes="20vw"
+              className="object-cover rounded-md"
+            />
+          </div>
+          <h1 className='mt-4 font-light text-cl tracking-wide text-center'>Category Name</h1>
+        </Link>
+        <Link href="/list?category=test" className='flex-shrink-0 w-full sm:w-1/2 lg:w-1/4 xl:w-1/6'>
+          <div className='relative bg-slate-100 w-full h-96'>
+            <Image
+              src="/img6.webp"
+              alt="Category Name"
+              fill
+              sizes="20vw"
+              className="object-cover rounded-md"
+            />
+          </div>
+          <h1 className='mt-4 font-light text-cl tracking-wide text-center'>Category Name</h1>
+        </Link>
+        <Link href="/list?category=test" className='flex-shrink-0 w-full sm:w-1/2 lg:w-1/4 xl:w-1/6'>
+          <div className='relative bg-slate-100 w-full h-96'>
+            <Image
+              src="/img6.webp"
+              alt="Category Name"
+              fill
+              sizes="20vw"
+              className="object-cover rounded-md"
+            />
+          </div>
+          <h1 className='mt-4 font-light text-cl tracking-wide text-center'>Category Name</h1>
+        </Link>
+        <Link href="/list?category=test" className='flex-shrink-0 w-full sm:w-1/2 lg:w-1/4 xl:w-1/6'>
+          <div className='relative bg-slate-100 w-full h-96'>
+            <Image
+              src="/img6.webp"
+              alt="Category Name"
+              fill
+              sizes="20vw"
+              className="object-cover rounded-md"
+            />
+          </div>
+          <h1 className='mt-4 font-light text-cl tracking-wide text-center'>Category Name</h1>
+        </Link>
         <Link href="/list?category=test" className='flex-shrink-0 w-full sm:w-1/2 lg:w-1/4 xl:w-1/6'>
           <div className='relative bg-slate-100 w-full h-96'>
             <Image
